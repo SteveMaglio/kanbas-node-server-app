8a2ac85b5c48609511f6a5727d74741188afda1d
SteveMaglio <magliocchino.s@northeastern.edu>
2023-12-04 16:50:53 -0500

    ern

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
-import express from 'express'
-import Hello from "./hello.js"
-import lab5 from "./lab5.js"
+import express from "express";
+import Hello from "./hello.js";
+import lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
+mongoose.connect("mongodb://localhost:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+
+
 app.use(express.json());
 ModuleRoutes(app);
-lab5(app)
-Hello(app)
+lab5(app);
+Hello(app);
 CourseRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
